5caacb5734ee8c8d815c9d81760c270153303d5e
Jonathan Palardy <jonathan.palardy@gmail.com>
2018-03-04 19:36:32 -0800

    memora large: use better Japanese font

--- src/memoraLarge.js
+++ src/memoraLarge.js
@@ -1,17 +1,15 @@
-
-/* jshint browser: true */
+/* global document */
 
 // some unique id, the SHA1 of "memoraLarge"
-var id = "d31320f0a68c10ee2dea47aa32b7d865d4e54e10";
-var d = document;
+const id = "d31320f0a68c10ee2dea47aa32b7d865d4e54e10";
+const d = document;
 
-var style = d.getElementById(id);
+const style = d.getElementById(id);
 if (style) {
   d.head.removeChild(style);
 } else {
-  style = d.createElement('style');
-  style.innerHTML = ".card .text { font-size: 300% }";
+  const style = d.createElement("style");
+  style.innerHTML = ".card .text { font-size: 300%; font-family: 'Hiragino Kaku Gothic ProN' }";
   style.id = id;
   d.head.appendChild(style);
 }
-
