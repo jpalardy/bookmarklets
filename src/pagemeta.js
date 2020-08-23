d34e4927ced1619c2f0fd0012bf507f5eac63e1e
Jonathan Palardy <jonathan.palardy@gmail.com>
2020-08-22 17:01:22 -0700

    Update pagemeta
    - IIFE
    - font-size

--- src/pagemeta.js
+++ src/pagemeta.js
@@ -1,15 +1,21 @@
-const today = new Date().toISOString().replace(/T.*/, "");
-const url = location.toString();
-const title = document.querySelector("title").innerText;
+(function () {
+  if (!navigator.clipboard) {
+    alert("clipboard not found!");
+    return;
+  }
 
-const blob = `
+  const today = new Date().toISOString().replace(/T.*/, "");
+  const url = location.toString();
+  const title = document.querySelector("title").innerText;
+
+  const blob = `
 ${today}
   ${url}
     ${title}
 `.trim();
 
-const banner = document.createElement("div");
-banner.style = `
+  const banner = document.createElement("div");
+  banner.style = `
   position: fixed;
   bottom: 0;
   left: 0;
@@ -18,13 +24,15 @@ banner.style = `
   background: #bbb;
   border-top: 1px solid #777;
   font-family: monospace;
+  font-size: 16px;
   z-index: 9999;
 `;
-banner.innerText = blob;
-banner.onclick = () => {
-  navigator.clipboard
-    .writeText(blob)
-    .then(() => banner.remove())
-    .catch(() => alert("clipboard copy failed"));
-};
-document.body.appendChild(banner);
+  banner.innerText = blob;
+  banner.onclick = () => {
+    navigator.clipboard
+      .writeText(blob)
+      .then(() => banner.remove())
+      .catch(() => alert("clipboard copy failed"));
+  };
+  document.body.appendChild(banner);
+})();
