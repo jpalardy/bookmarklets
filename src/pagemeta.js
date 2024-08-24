c935c6ccf6e469372ba64fa39bbafdaf30a678b3
Jonathan Palardy <jonathan.palardy@gmail.com>
2024-08-24 16:40:17 -0700

    Inline pagemeta without .src punt

--- src/pagemeta.js
+++ src/pagemeta.js
@@ -1,3 +1,58 @@
-const s = document.createElement("script");
-s.src = "https://home.jpalardy.com/bookmarklets/pagemeta.js";
-document.body.appendChild(s);
+(() => {
+  const now = new Date();
+  const title = (document.querySelector("title") || { innerText: "" }).innerText.trim().replace(/\s+/g, " ");
+  const url = document.location.toString();
+  const copyText = `${title}\n  ${url}`;
+
+  const banner = document.createElement("div");
+  banner.className = "pagemeta";
+
+  const style = document.createElement("style");
+  style.textContent = `
+.pagemeta {
+  all: revert;
+  position: fixed;
+  bottom: 0;
+  left: 0;
+  width: 100%;
+  background: rgb(0, 0, 0, 0.7);
+  z-index: 9999;
+}
+.pagemeta div {
+  all: revert;
+  padding: 20px;
+  color: white;
+  font-family: monospace;
+  font-size: 16px;
+  text-align: left;
+  white-space: pre;
+}
+.pagemeta button {
+  all: revert;
+  position: absolute;
+  top: 10px;
+  right: 10px;
+}`;
+  document.head.appendChild(style);
+
+  const textDiv = document.createElement("div");
+  textDiv.textContent = copyText;
+  banner.appendChild(textDiv);
+
+  const button = document.createElement("button");
+  button.textContent = navigator.clipboard ? "copy and close" : "close";
+  banner.appendChild(button);
+
+  document.body.appendChild(banner);
+
+  button.onclick = () => {
+    if (!navigator.clipboard) {
+      banner.remove();
+      return;
+    }
+    navigator.clipboard
+      .writeText(copyText)
+      .then(() => banner.remove())
+      .catch(() => alert("clipboard copy failed"));
+  };
+})();
