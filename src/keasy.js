39ae34170cf4a1abac3f6d99321b2c892de938b0
Jonathan Palardy <jonathan.palardy@gmail.com>
2025-03-09 13:47:31 -0700

    Tweak keasy
    - 0..1 kanjis
    - without noma
    - use outline

--- src/keasy.js
+++ src/keasy.js
@@ -1,10 +1,12 @@
 (() => {
-  const cards = [...document.querySelectorAll(".card")];
-  cards.forEach((card) => {
+  [...document.querySelectorAll(".card")].forEach((card) => {
     const text = card.querySelector("span").innerText;
-    const onlyKanjis = text.replace(/[^\p{Script=Han}]/gu, "");
-    if (onlyKanjis.length === 1) {
-      card.style.backgroundColor = "#FFFBCC";
+    const onlyKanjis = text
+      .replace(/[^\p{Script=Han}]/gu, "")
+      .replace(/々/g, "");
+    // 1 kanji or 0 kanjis (hiragana, katakana only)
+    if (onlyKanjis.length <= 1) {
+      card.style.boxShadow = "0 0 7px red";
     }
   });
 })();
