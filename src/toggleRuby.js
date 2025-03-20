9b781ddf26defb0258977e705524b06303edaa5d
Jonathan Palardy <jonathan.palardy@gmail.com>
2025-03-20 10:52:46 -0700

    Rework toggle-ruby
    - code structure, slightly
    - hover to see rt

--- src/toggleRuby.js
+++ src/toggleRuby.js
@@ -1,13 +1,15 @@
 // some unique id, the SHA1 of "toggleRuby"
 const id = "c8f38bbf013e6f254dfe129984188c9a2646b793";
-const d = document;
 
-const style = d.getElementById(id);
+const style = document.getElementById(id);
 if (style) {
-  d.head.removeChild(style);
+  document.head.removeChild(style);
 } else {
-  const style = d.createElement("style");
-  style.innerHTML = "rt { visibility: hidden; }";
+  const style = document.createElement("style");
+  style.innerHTML = `
+    rt { visibility: hidden; }
+    ruby:hover rt { visibility: visible; }
+  `;
   style.id = id;
-  d.head.appendChild(style);
+  document.head.appendChild(style);
 }
