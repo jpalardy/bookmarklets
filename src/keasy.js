851f7f7244afd22ed8ca8fc3c3e4bed47d1d8a07
Jonathan Palardy <jonathan.palardy@gmail.com>
2025-03-29 11:34:42 -0700

    Exclude number kanjis #keasy

--- src/keasy.js
+++ src/keasy.js
@@ -3,7 +3,8 @@
     const text = card.querySelector("span").innerText;
     const onlyKanjis = text
       .replace(/[^\p{Script=Han}]/gu, "")
-      .replace(/々/g, "");
+      .replace(/々/g, "")
+      .replace(/[一二三四五六七八九十]/g, "");
     // 1 kanji or 0 kanjis (hiragana, katakana only)
     if (onlyKanjis.length <= 1) {
       card.style.boxShadow = "0 0 7px red";
