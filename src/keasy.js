e130c9785e7bd82a5622330556ecbc8f82e20a69
Jonathan Palardy <jonathan.palardy@gmail.com>
2026-02-17 10:40:50 -0800

    Try text-shadow for keasy highlighting

--- src/keasy.js
+++ src/keasy.js
@@ -1,13 +1,11 @@
 (() => {
   function highlight(elem, color) {
-    elem.style.background = color;
-    elem.style.padding = "15px";
-    elem.style.borderRadius = "999px";
+    elem.style.textShadow = `0 0 20px ${color}`;
   }
   [...document.querySelectorAll(".card")].forEach((card) => {
     const span = card.querySelector("span");
     if (span.innerText.length == 1) {
-      highlight(span, "#FFFBCC");
+      highlight(span, "cyan");
       return;
     }
     const onlyKanjis = span.innerText
@@ -16,7 +14,7 @@
       .replace(/[一二三四五六七八九十]/g, "");
     // 1 kanji or 0 kanjis (hiragana, katakana only)
     if (onlyKanjis.length <= 1) {
-      highlight(span, "lightblue");
+      highlight(span, "yellow");
       return;
     }
   });
