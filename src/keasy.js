f670b1f172921af4c6320e2139721b62fa718eb4
Jonathan Palardy <jonathan.palardy@gmail.com>
2026-03-14 12:26:41 -0700

    Highlight (red) "long-time since reviewed" cards

--- src/keasy.js
+++ src/keasy.js
@@ -4,10 +4,12 @@
   }
   [...document.querySelectorAll(".card")].forEach((card) => {
     const span = card.querySelector("span");
+    // one kanji (only)
     if (span.innerText.length == 1) {
       highlight(span, "cyan");
       return;
     }
+    // one kanji (after trim)
     const onlyKanjis = span.innerText
       .replace(/[^\p{Script=Han}]/gu, "")
       .replace(/々/g, "")
@@ -15,6 +17,13 @@
     // 1 kanji or 0 kanjis (hiragana, katakana only)
     if (onlyKanjis.length <= 1) {
       highlight(span, "yellow");
+      return;
+    }
+    // been a while
+    const preview = card.querySelector(".preview");
+    const daysAgo = Number(preview.innerHTML.split(" ")[0]);
+    if (daysAgo >= 60) {
+      highlight(span, "red");
       return;
     }
   });
