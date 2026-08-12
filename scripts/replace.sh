1987870ab89c03d22b4b2aa453f5ea6ed65b19ea
Jonathan Palardy <jonathan.palardy@gmail.com>
2026-08-12 11:02:27 -0700

    Add kill-sticky

--- scripts/replace.sh
+++ scripts/replace.sh
@@ -5,5 +5,6 @@ cat "$@" | \
          -e "/amazon-ca/ s/#code/$(npm exec bookmarklet src/amazon-ca.js)/" \
          -e "/pagemeta/ s/#code/$(npm exec bookmarklet src/pagemeta.js)/" \
          -e "/highlight-yellow/ s/#code/$(npm exec bookmarklet src/highlight-yellow.js)/" \
-         -e "/keasy[^-]/ s/#code/$(npm exec bookmarklet src/keasy.js)/"
+         -e "/keasy/ s/#code/$(npm exec bookmarklet src/keasy.js)/" \
+         -e "/kill-sticky/ s/#code/$(npm exec bookmarklet src/kill-sticky.js)/"
 
---
+++ src/kill-sticky.js
@@ -0,0 +1,24 @@
+// https://github.com/t-mart/kill-sticky
+document.querySelectorAll("body *").forEach(function (node) {
+  if (["fixed", "sticky"].includes(getComputedStyle(node).position)) {
+    node.parentNode.removeChild(node);
+  }
+});
+
+document.querySelectorAll("html *").forEach(function (node) {
+  var s = getComputedStyle(node);
+  if ("hidden" === s["overflow"]) {
+    node.style["overflow"] = "visible";
+  }
+  if ("hidden" === s["overflow-x"]) {
+    node.style["overflow-x"] = "visible";
+  }
+  if ("hidden" === s["overflow-y"]) {
+    node.style["overflow-y"] = "visible";
+  }
+});
+
+var htmlNode = document.querySelector("html");
+htmlNode.style["overflow"] = "visible";
+htmlNode.style["overflow-x"] = "visible";
+htmlNode.style["overflow-y"] = "visible";
