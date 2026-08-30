dd3d8a7cd67799ecd8ea2b7f9a95097a284a9d8c
Jonathan Palardy <jonathan.palardy@gmail.com>
2026-08-30 14:20:16 -0400

    Rework pagemeta completely
    - use popover
    - simpler HTML, CSS
    - add "copy markdown"

--- src/pagemeta.js
+++ src/pagemeta.js
@@ -1,58 +1,52 @@
 (() => {
-  const now = new Date();
-  const title = (document.querySelector("title") || { innerText: "" }).innerText.trim().replace(/\s+/g, " ");
-  const url = document.location.toString();
-  const copyText = `${title}\n  ${url}`;
-
-  const banner = document.createElement("div");
-  banner.className = "pagemeta";
+  let pagemeta = document.querySelector('.pagemeta');
+  if (pagemeta) {
+    pagemeta.showPopover();
+    return;
+  }
 
   const style = document.createElement("style");
   style.textContent = `
-.pagemeta {
-  all: revert;
-  position: fixed;
-  bottom: 0;
-  left: 0;
-  width: 100%;
-  background: rgb(0, 0, 0, 0.7);
-  z-index: 9999;
-}
-.pagemeta div {
-  all: revert;
-  padding: 20px;
-  color: white;
-  font-family: monospace;
-  font-size: 16px;
-  text-align: left;
-  white-space: pre;
-}
-.pagemeta button {
-  all: revert;
-  position: absolute;
-  top: 10px;
-  right: 10px;
-}`;
+    .pagemeta {
+      all: revert;
+      font-family: monospace;
+      padding: 10px;
+      min-width: 400px;
+      width: fit-content;
+      max-width: 70vw;
+      overflow: auto;
+      border-radius: 7px;
+    }
+    [popover]::backdrop {
+      background: rgb(0 0 0 / 0.8);
+    }`;
   document.head.appendChild(style);
 
-  const textDiv = document.createElement("div");
-  textDiv.textContent = copyText;
-  banner.appendChild(textDiv);
+  let title = (document.querySelector("title") || { innerText: "" }).innerText.trim().replace(/\s+/g, " ");
+  const url = document.location.toString();
 
-  const button = document.createElement("button");
-  button.textContent = navigator.clipboard ? "copy and close" : "close";
-  banner.appendChild(button);
+  pagemeta = document.createElement("div");
+  pagemeta.popover = "auto";
+  pagemeta.className = "pagemeta";
 
-  document.body.appendChild(banner);
+  const content = document.createElement('div');
+  content.innerText = `${title}\n${url}`;
+  pagemeta.appendChild(content);
 
-  button.onclick = () => {
-    if (!navigator.clipboard) {
-      banner.remove();
-      return;
-    }
-    navigator.clipboard
-      .writeText(copyText)
-      .then(() => banner.remove())
-      .catch(() => alert("clipboard copy failed"));
-  };
+  if (navigator.clipboard) {
+    content.style.marginBottom = "5px";
+    [
+      { text: "copy text", payload: `${title}\n  ${url}` },
+      { text: "copy markdown", payload: `[${title}](${url})` }
+    ].forEach((props) => {
+      const button = document.createElement("button");
+      button.textContent = props.text;
+      button.style.marginRight = "5px";
+      button.onclick = () => { navigator.clipboard.writeText(props.payload) };
+      pagemeta.appendChild(button);
+    });
+  }
+
+  document.body.appendChild(pagemeta);
+  pagemeta.showPopover();
 })();
